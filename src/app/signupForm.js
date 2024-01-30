61696b356ee41dea77640b3a749d92aa9d5afd3e
Cristian <cristiankhan23@gmail.com>
2024-01-29 22:47:15 -0300

    Handling errors and add Toastify

--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -1,5 +1,7 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js"
 import { auth } from './firebase.js'
+import { showMessage } from './showMessage.js'
+
 
 const signupForm = document.querySelector('#signup-form')
 
@@ -13,13 +15,30 @@ signupForm.addEventListener('submit', async (e) => {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password)
     console.log(userCredential)
      
-    // Close signup modal
-    const signupModal = document.querySelector('#signupModal')
-    const modal = bootstrap.Modal.getInstance(signupModal)
-    modal.hide()
-    
-  } catch (error) {
-    console.log(error)
-  }
+    closeModal('#signupModal')
 
+    // Show toastify message
+    const wellcome = "Wellcome " + userCredential.user.email
+    showMessage(wellcome)
+
+  } catch (error) {
+
+    // Handling errors
+    if (error.code === 'auth/email-already-in-use') {
+      showMessage("Email already in use", "error")
+    } else if (error.code === 'auth/weak-password') {
+      showMessage("Password should be at least 6 characters", "error")
+    } else if (error.code === 'auth/invalid-email') {
+      showMessage("Invalid email", "error")
+    } else if (error.code) {
+      showMessage("Something went wrong", "error")
+    } 
+  }
 })
+
+function closeModal(id) {
+  // Close signup modal
+  const signupModal = document.querySelector(id)
+  const modal = bootstrap.Modal.getInstance(signupModal)
+  modal.hide()
+}
