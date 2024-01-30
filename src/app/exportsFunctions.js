520ab81ac855df060c1fbbd647143760b17d49bf
Cristian <cristiankhan23@gmail.com>
2024-01-30 16:43:39 -0300

    Exports functions

---
+++ src/app/exportsFunctions.js
@@ -0,0 +1,25 @@
+
+// Show Toastify messages
+export function showMessage(msg, type = "success") {
+  Toastify({
+    text: msg,
+    duration: 4000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: type === "success" ? "green" : "red",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast(); 
+}
+
+// Close signin or signup modal
+export function closeModal(id) {
+  const signinUp = document.querySelector(id)
+  const modal = bootstrap.Modal.getInstance(signinUp)
+  modal.hide()
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-import './app/signupForm.js';
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js"
+import { auth } from './app/firebase.js'
+import { loginCheck } from './app/loginCheck.js'
+import './app/signupForm.js'
+import './app/logout.js'
+import './app/signinForm.js'
+
+onAuthStateChanged(auth, async (user) => {
+  loginCheck(user)
+})
