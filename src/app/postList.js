efa264795e56c217e3fe9a03b0c2bc3de2e481b0
Cristian <cristiankhan23@gmail.com>
2024-02-02 13:40:24 -0300

    New functionality for do posts

--- src/app/postList.js
+++ src/app/postList.js
@@ -2,12 +2,16 @@ const postList = document.querySelector('.posts')
 
 export const setupPosts = (data) => {
   if (data.length) {
+    // Order the docs by timestamp
+    data.sort((a, b) => b.data().timestamp - a.data().timestamp)
+    
     let html = ''
+    
     // for each document in the db
     data.forEach(doc => {
       const post = doc.data()
       const li = `
-        <li class="list-group-item list-group-item-action list-group-item-dark">
+        <li class="list-group-item flex justify-content-between list-group-item-action text-dark">
           <h5>${post.title}</h5>
           <p>${post.content}</p>
         </li>
@@ -16,6 +20,6 @@ export const setupPosts = (data) => {
     })
     postList.innerHTML = html
   } else {
-    postList.innerHTML = '<h1>Login to see the posts</h1>'
+    postList.innerHTML = '<h1 class="text-white text-center">Login to see the posts</h1>'
   }
 }
--- src/app/posts.js
+++ src/app/posts.js
@@ -1,7 +1,6 @@
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js"
+import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js"
 import { closeModal, showMessage } from './exportsFunctions.js'
 import { db } from './firebase.js'
-
 
 const postForm = document.querySelector('#post-form')
 
@@ -9,21 +8,25 @@ postForm.addEventListener('submit', async (e) => {
   e.preventDefault()
 
   try {
-
     const title = postForm['post-title'].value
     const content = postForm['post-text'].value
     
+    if (!title || !content) {
+      throw new Error('Title or content inputs cannot be empty')
+    }
+    // add new document in the data base
     await addDoc(collection(db, 'posts'), {
       title,
-      content
+      content,
+      timestamp: serverTimestamp(),
     })
 
     closeModal('#postModal')
-    showMessage('You post was successfully!')
     window.location.replace('/src/index.html')
-    
-  } catch (error) {
-   console.log(error.message) 
-  }
 
+  } catch (error) {
+   if (error.message) {
+    showMessage("Something went wrong", "error")
+   }
+  }
 })
